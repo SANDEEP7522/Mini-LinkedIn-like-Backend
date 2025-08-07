b4eb4ecfa04bc10ac472c58d14816dec9868ef7d
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2025-08-07 19:34:02 +0530

    add

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [FRONTEND_URL],
     credentials: true
   })
 );
